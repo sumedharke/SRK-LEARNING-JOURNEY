cd458d7132fa1b637e6ef1096c4086e28163a506
sumedharke <sumedharke123@gmail.com>
2026-04-07 23:40:31 +0530

    Trying to Make Calculater

--- Python for Everybody (PY4E).py
+++ Python for Everybody (PY4E).py
@@ -7,3 +7,45 @@ X = X + 5
 print(X)
 
 print("Hello World")
+
+
+print("Constant")
+print("I am Successful I am become a Great Programmer & I am going to be a Data Scientist & I am Going to Biggest Man in This World & Highest Earner")
+
+Variable = "Variable is a container for a value that can change & Store data that can be used Later in the program"
+
+print(Variable)
+
+print("Reserved Words")
+print("False, None, True, and, as, assert, break, class, continue, def, del, elif, else, except, exec, finally, for, from, global, if, import, in, is, lambda, not, or, pass, print, raise, return, try, while, with, yield")
+
+print("Identifiers")
+print("Names used to identify variables, functions, classes, modules, and other objects in Python")
+print("Rules for Python Identifiers")
+print("- Must start with a letter (A-Z, a-z) or an underscore (_)")
+print("- Can contain letters, digits (0-9), and underscores")
+print("- Are case-sensitive")
+print("- Cannot be reserved words")
+
+Assignments_Statements = "Assignments are tasks or exercises given to students to practice and apply what they have learned in a course or class"
+print(Assignments_Statements)
+
+# Make Calculator Project 
+
+print("Welcome to the Calculator Project!")
+print("This calculator can perform basic arithmetic operations: addition, subtraction, multiplication, and division.") 
+
+# Get user input for the First Number
+
+number1 = int(input("Enter the first number: "))
+
+# Get user input for the second number
+    
+number2 = int(input("Enter the second Number: "))
+
+# Get User input for the operation they want to perform
+
+operation = input("Enter the operation you want to perform (+, -, *, /): ")
+
+# Perform the calculation based on the operation - Lots of Options/Methods to do this but I am using if-else statement ^ Then Simple if-else statement to perfrom the calculation based on the operaction enter
+
