a2b4ab0ef077d1a225262ec94b27b22efaa0e018
sumedharke <sumedharke123@gmail.com>
2026-05-04 01:17:21 +0530

    New Code Add in Restart

--- Day - 1_Restart.py
+++ Day - 1_Restart.py
@@ -2,3 +2,8 @@ name = "SRK"
 city = "Sillod"
 mission = "Remote developer - stay with my parents forever"
 print(name, city, mission)
+
+age = 27
+is_developer = False
+print("Currentrly a developer:", is_developer)
+print("Will be a developer:", True)
