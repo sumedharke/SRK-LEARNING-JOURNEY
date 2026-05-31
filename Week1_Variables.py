3b3567606fc81a05e8ab6205b1f57d4c8f105b88
sumedharke <sumedharke123@gmail.com>
2026-05-31 23:45:34 +0530

    Week 1 Day 1

---
+++ Week1_Variables.py
@@ -0,0 +1,19 @@
+# Week 1 - Variables and Data Types
+
+
+# Variables
+
+name = "SRK"
+age = 28
+height = 5.8
+is_developer = False
+City = "Sillod"
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_developer))
+
+# Data Types
+
