db8eaa0d6e23b1620197a09a1461b3179e80889e
sumedharke <sumedharke123@gmail.com>
2026-06-01 00:43:55 +0530

    1st Day

--- Week1_Variables.py
+++ Week1_Variables.py
@@ -10,10 +10,23 @@ is_developer = False
 City = "Sillod"
 
 
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_developer))
+print(type(name))               # string
+print(type(age))                # integer
+print(type(height))             # float
+print(type(is_developer))       # boolean
 
 # Data Types
+
+# Arthmetic Operators
+
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a % b)   # Modulus
+print(a ** b)  # Exponetiation
+print(a // b)  # Floor Division
 
